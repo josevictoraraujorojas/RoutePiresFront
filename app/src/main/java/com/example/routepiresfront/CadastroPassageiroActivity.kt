f507aefecc6d6709995737b031e2b02c0b39241f
Cristian <cristianmarfer14@gmail.com>
2025-10-29 08:31:46 -0300

    feat: Disable finish button after click
    Disables the `btnFinalizar` button after it has been clicked to prevent multiple submissions.

--- app/src/main/java/com/example/routepiresfront/CadastroPassageiroActivity.kt
+++ app/src/main/java/com/example/routepiresfront/CadastroPassageiroActivity.kt
@@ -44,6 +44,7 @@ class CadastroPassageiroActivity : AppCompatActivity() {
                     // Aqui você pode enviar os dados para API ou banco local
                 }
             }
+            btnFinalizar.isEnabled = false
         }
     }
 
