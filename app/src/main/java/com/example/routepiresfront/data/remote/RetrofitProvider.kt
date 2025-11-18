2e9300b42b3ff2f94a4d0eadc64025d820ccf1e2
josevictoraraujorojas <kaneki1212112121@gmail.com>
2025-11-18 14:42:01 -0300

    Api

--- app/src/main/java/com/example/routepiresfront/data/remote/RetrofitProvider.kt
+++ app/src/main/java/com/example/routepiresfront/data/remote/RetrofitProvider.kt
@@ -6,7 +6,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitProvider {
-    private const val BASE_URL = "http://10.0.2.2:8080/" // ajuste aqui
+    private const val BASE_URL = "http://192.168.0.103:8080/" // ajuste aqui
 
     fun createApi(): MototaxistaApi {
         val logging = HttpLoggingInterceptor().apply {
