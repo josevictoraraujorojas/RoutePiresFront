6348b2688f55c238dcb5dc6f59f463059635ad04
Rodrigo <rodrigoip435@gmail.com>
2025-11-22 20:29:16 -0300

    refactor: Usar activityViewModels para compartilhar dados
    Altera a inicialização do ViewModel de `viewModels` para `activityViewModels`, permitindo o compartilhamento de dados entre fragments.

--- app/src/main/java/com/example/routepiresfront/ui/mototaxista/ConfiguracaoMotoristaFragment.kt
+++ app/src/main/java/com/example/routepiresfront/ui/mototaxista/ConfiguracaoMotoristaFragment.kt
@@ -7,7 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
+import androidx.fragment.app.activityViewModels
 import androidx.navigation.findNavController
 import coil.load
 import com.example.routepiresfront.R
@@ -21,7 +21,7 @@ class ConfiguracaoMotoristaFragment : Fragment() {
     private var _binding: FragmentConfiguracaoMotoristaBinding? = null
     private val binding get() = _binding!!
 
-    private val viewModel: MototaxistaPerfilViewModel by viewModels()
+    private val viewModel: MototaxistaPerfilViewModel by activityViewModels()
 
     private var userId: String? = null
 
